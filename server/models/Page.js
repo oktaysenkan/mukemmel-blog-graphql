df7cb38c5af871eecc223c0de46bb4f6d3e04451
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-21 14:26:05 +0300

    Page model implementasyonu yapıldı.

---
+++ server/models/Page.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { ObjectID } from "mongodb";
+
+const Schema = mongoose.Schema;
+
+ObjectID.prototype.valueOf = function() {
+  return this.toString();
+};
+
+const PageSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Page", PageSchema);
